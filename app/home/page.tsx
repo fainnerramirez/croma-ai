37c957997a6281c23be1c943946a1772c3b9a1f9
fainnerramirez <fainnerramirez@gmail.com>
2026-06-30 17:15:48 -0500

    feat: add project nextjs, tailwind css and hero ui

---
+++ app/home/page.tsx
@@ -0,0 +1,18 @@
+
+export default function HomePage() {
+    return (
+        <div className="">
+            <div className="">
+                <h1 className="">
+                    Encuentra la paleta perfecta para cualquier proyecto
+                </h1>
+                <h3>
+                    Desde una simple descripción, la IA genera colores listos para usar en diseño, branding o desarrollo.
+                </h3>
+            </div>
+            <div className="">
+                <img src={"./home.png"} alt="home image" />
+            </div>
+        </div>
+    )
+}
